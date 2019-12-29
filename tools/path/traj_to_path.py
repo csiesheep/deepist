ec4c71ee2c16ab8ec420f1548d2beda8cbf55a28
csiesheep <csiegoat@gmail.com>
2019-12-29 15:54:57 -0500

    (tools) add road ids in paths

--- tools/path/traj_to_path.py
+++ tools/path/traj_to_path.py
@@ -22,6 +22,9 @@ def main(fname, matched_folder, output_fname):
         with open(output_fname, 'w') as fo:
             for line in f:
                 ith += 1
+                if ith % 100 == 0:
+                    print ith, count, no_count
+
                 id_, txys, time, timestamp = line.strip().split('\t')
                 matched = load_matched(matched_folder, id_)
 #               matched = load_matched(matched_folder, ith)
@@ -29,38 +32,46 @@ def main(fname, matched_folder, output_fname):
                 if matched is None:
                     no_count += 1
                     continue
-                else:
-                    txys = eval(txys)
 
-                    count += 1
+                txys = eval(txys)
+                count += 1
+                new_txys = []
+                for txy, m in zip(txys, matched):
 
-                    new_txys = []
-                    try:
-                        for txy, m in zip(txys, matched):
-
-                            if len(new_txys) == 0:
-                                new_x, new_y = map(float, m['geom'][7:-1].split(' '))
-                                new_txys.append((txy[0], new_x, new_y))
-                            else:
-                                diff_t = txy[0] - new_txys[-1][0]
-                                new_xys = []
-                                for s in m['geom'][12:-1].split(', '):
-                                    new_x, new_y = map(float, s.split(' '))
-                                    new_xys.append((new_x, new_y))
-                                times = split_time(new_txys[-1][0], diff_t, new_xys)
-
-                                for t, (x, y) in zip(times, new_xys[1:]):
-                                    new_txys.append((t, x, y))
-
-                        fo.write('%s\t%s\t%s\t%s\n' % (id_, new_txys, time, timestamp))
-                    except:
+                    if len(new_txys) == 0:
+                        new_x, new_y = map(float, m['geom'][7:-1].split(' '))
+                        new_txys.append((txy[0],
+                                         new_x,
+                                         new_y,
+                                         '%d-%s' % (m['point']['road'],
+                                                    m['point']['heading'])))
                         continue
 
-                if ith % 100 == 0:
-                    print ith, count, no_count
+                    diff_t = txy[0] - new_txys[-1][0]
+                    new_xys = []
+                    for s in m['geom'][12:-1].split(', '):
+                        new_x, new_y = map(float, s.split(' '))
+                        new_xys.append((new_x, new_y))
+                    times = split_time(new_txys[-1][0], diff_t, new_xys)
+
+                    if len(new_xys) == 1:
+                        new_txys.append((new_txys[-1][0]+diff_t,
+                                         new_xys[-1][0],
+                                         new_xys[-1][1],
+                                         '%d-%s' % (m['point']['road'],
+                                                    m['point']['heading'])))
+                        continue
+                    for t, (x, y) in zip(times, new_xys[1:-1]):
+                        new_txys.append((t, x, y, None))
+                    new_txys.append((times[-1],
+                                     new_xys[-1][0],
+                                     new_xys[-1][1],
+                                     '%d-%s' % (m['transition']['route']['target']['road'],
+                                                m['transition']['route']['target']['heading'])))
+
+                fo.write('%s\t%s\t%s\t%s\n' % (id_, new_txys, time, timestamp))
 
     print count, no_count
-
     return 0
 
 def split_time(base_t, diff_t, new_xys):
@@ -79,7 +90,6 @@ def split_time(base_t, diff_t, new_xys):
 def load_matched(matched_folder, id_):
     fname = os.path.join(matched_folder, '%s.json' % id_)
     if not os.path.exists(fname):
-        print fname
         return None
     try:
         with open(fname) as f:
@@ -94,22 +104,6 @@ def load_matched(matched_folder, id_):
         return None
     except SyntaxError:
         return None
-
-def load_trajs(fname):
-    trajs = {}
-    ith = 0
-    with open(fname) as f:
-        for line in f:
-            _, txys, _ = line.split('\t')
-            traj = eval(txys)
-            times, lats, lons = [], [], []
-            for (t, lon, lat) in traj:
-                lats.append(lat)
-                lons.append(lon)
-                times.append(t)
-            trajs[ith] = (lons, lats, times)
-            ith += 1
-    return trajs
 
 
 if __name__ == '__main__':
